ed7bfdf79bc78412260d6df60eda2f94c23e90f1
Bartosz Kąkol <bartosz.kakol@proton.me>
2024-01-22 10:22:16 +0100

    moved board assets, moved movement calculation functions to separate file

--- scenes/gameplay.py
+++ scenes/gameplay.py
@@ -1,8 +1,6 @@
-import pygame
-
-import calc
 from scene import Scene, SceneController
 from sprites import Player, CompleteBoard
+from .utils.gameplay import *
 
 
 class GameplayScene(Scene):
@@ -10,7 +8,7 @@ class GameplayScene(Scene):
     player: Player
 
     def start(self):
-        self.board = CompleteBoard((self.assets["plansza_bg"], 1), (self.assets["plansza_fg"], 3), self.screen_rect)
+        self.board = CompleteBoard(self.assets["plansza"], self.screen_rect, z_index=(1, 3))
         self.sprites.append(self.board)
 
         self.player = Player(self.assets["kot"]["D0"], self.screen_rect, z_index=2)
@@ -18,7 +16,7 @@ class GameplayScene(Scene):
 
     def update(self, controller: 'SceneController'):
         keys = pygame.key.get_pressed()
-        board_move, self.player.direction = calc.movement(keys)
+        board_move, self.player.direction = calculate_board_movement(keys)
         player_move = [0, 0]
 
         if board_move[0] != 0 or board_move[1] != 0:
@@ -28,6 +26,6 @@ class GameplayScene(Scene):
 
         self.player.image = self.assets["kot"][f"{self.player.direction}{self.player.animation_frame}"]
 
-        calc.redirect_board_movement_to_player(board_move, player_move, self.board.bg.rect, self.player.rect, self.screen_rect, self.board.bg.max_pos)
+        redirect_board_movement_to_player(board_move, player_move, self.board.bg.rect, self.player.rect, self.screen_rect, self.board.bg.max_pos)
         self.board.move(*board_move)
         self.player.move(*player_move)
--- sprites/completeboard.py
+++ sprites/completeboard.py
@@ -5,16 +5,17 @@ from sprites import BoardFragment
 
 
 class CompleteBoard(MultiSprite):
-    def __init__(self, board_bg: (pygame.Surface, int), board_fg: (pygame.Surface, int), screen_rect: pygame.Rect):
+    def __init__(self, board_assets: dict[str, pygame.Surface], screen_rect: pygame.Rect, *, z_index: tuple[int, int]):
         self.contained_sprites = {
-            "bg": BoardFragment(board_bg[0], screen_rect, z_index=board_bg[1]),
-            "fg": BoardFragment(board_fg[0], screen_rect, z_index=board_fg[1])
+            "bg": BoardFragment(board_assets["bg"], screen_rect, z_index=z_index[0]),
+            "fg": BoardFragment(board_assets["fg"], screen_rect, z_index=z_index[1])
         }
 
     def move(self, x: float, y: float):
         self.contained_sprites["bg"].move(x, y)
         self.contained_sprites["fg"].move(x, y)
 
+    # noinspection PyTypeChecker
     @property
     def bg(self) -> BoardFragment:
         return self.contained_sprites["bg"]
